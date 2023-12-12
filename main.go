8cde5bf5a2c605b45e359c1892be2289fcded6b5
Kuba Wieczorek <kuba.wieczorek@hashicorp.com>
2023-12-12 20:20:19 +0000

    Break the CI

--- main.go
+++ main.go
@@ -15,11 +15,11 @@ func main() {
 
 // Add takes two integers and returns the sum of them and a nil error if
 // everything goes right. The integers have to be non-negative, otherwise Add
-// will return a zero and an error.
+// will return a zero and an error!
 func Add(a, b int) (int, error) {
 	if a < 0 || b < 0 {
 		return 0, fmt.Errorf("a and b need to be non-negative integers")
 	}
 
-	return a + b, nil
+	return a - b, nil
 }
